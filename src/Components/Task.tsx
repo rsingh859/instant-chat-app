d413acc56ae1a54b00f5e7f8e141046ed191a154
rsingh859 <singh.ritesh2806@gmail.com>
2024-01-04 17:10:15 +0530

    Adding edit mode for tasks in UI

--- src/Components/Task.tsx
+++ src/Components/Task.tsx
@@ -1,6 +1,6 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useState } from "react";
 import Icons from "./Icons";
-import { MdDelete, MdEdit } from "react-icons/md";
+import { MdDelete, MdEdit, MdSave } from "react-icons/md";
 import { taskType } from "../Types";
 
 type Props = {
@@ -11,24 +11,49 @@ type Props = {
 const Task = forwardRef(
   ({ task, listId }: Props, ref: React.LegacyRef<HTMLDivElement>) => {
     const { id, title, description, editMode, collapsed } = task;
+    const [homeTitle, setHomeTitle] = useState(title);
+    const [homeDescription, setHomeDescription] = useState(description);
     return (
       <div
         ref={ref}
         className="p-2 mb-2 bg-[#f9fff0] rounded-md drop-shadow-sm hover:drop-shadow-md"
       >
         <div>
-          <p className="cursor-pointer">{title}</p>
+          {editMode ? (
+            <input
+              value={homeTitle}
+              onChange={(e) => setHomeTitle(e.target.value)}
+              className="border-2 p-2 border-myBlue rounded-sm mb-1"
+              placeholder="Task title"
+            />
+          ) : (
+            <p className="cursor-pointer">{title}</p>
+          )}
         </div>
-        <div>
-          <hr />
+        {!collapsed && (
           <div>
-            <p>{description}</p>
-            <div className="flex justify-end">
-              <Icons IconName={MdEdit} />
-              <Icons IconName={MdDelete} />
+            <hr />
+            <div>
+              {editMode ? (
+                <textarea
+                  value={homeDescription}
+                  placeholder="Todo descriptiion"
+                  className="w-full px-3 border-2 border-myBlue rounded-md mt-2"
+                  onChange={(e) => setHomeDescription(e.target.value)}
+                >
+                  {}
+                </textarea>
+              ) : (
+                <p className="p-2 text-justify">{description}</p>
+              )}
+
+              <div className="flex justify-end">
+                <Icons IconName={editMode ? MdSave : MdEdit} />
+                <Icons IconName={MdDelete} />
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     );
   }
